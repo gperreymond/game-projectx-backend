0963dc200f767e4f997b5de64abef11a6b5327d5
Gilles Perreymond <gperreymond@gmail.com>
2019-12-01 11:55:11 +0100

    feat(): initial commit, infra

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker network create -d overlay --attachable global
+
+if [ "$1" = '--start' ]; then
+  docker stack deploy -c infrastructure/docker-compose.traefik.yml proxy
+  docker stack deploy -c infrastructure/docker-compose.portainer.yml portainer
+  docker stack deploy -c infrastructure/docker-compose.databases.yml databases
+  docker stack deploy -c infrastructure/docker-compose.brokers.yml brokers
+fi
+
+if [ "$1" = '--start-grafana' ]; then
+  docker stack deploy -c infrastructure/docker-compose.grafana.yml grafana
+fi
+
+if [ "$1" = '--stop' ]; then
+  docker stack rm proxy
+  docker stack rm portainer
+  docker stack rm databases
+  docker stack rm brokers
+  docker stack rm grafana
+fi
